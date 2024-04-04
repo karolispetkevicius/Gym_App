3cde473b2bb86f08e202ff2708e4454681fa7610
Karolis <karolispetkevicius7@gmail.com>
2024-04-04 11:44:43 +0300

    clean up

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,8 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 
 import { createRoot } from 'react-dom/client';
 
